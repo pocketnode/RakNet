502b71d0bc834317c6da0bf61f3a8b660a9a6c86
eDroid <edroiiid@gmail.com>
2017-12-22 02:02:46 -0800

    lots o' changes

--- server/SessionManager.js
+++ server/SessionManager.js
@@ -1,11 +1,15 @@
 const Session = require("./Session");
-const TempSession = require("./TempSession");
 
 const OfflineMessage = require("../protocol/OfflineMessage");
 const OfflineMessageHandler = require("./OfflineMessageHandler");
 
+const BITFLAG = require("../protocol/BitFlags");
+const ACK = require("../protocol/ACK");
+const NACK = require("../protocol/NACK");
 const Datagram = require("../protocol/Datagram");
-const DatagramHandler = require("./DatagramHandler");
+
+const RAKNET_TPS = 100;
+const RAKNET_TICK_LENGTH = 1 / RAKNET_TPS;
 
 class SessionManager {
     initVars(){
@@ -17,19 +21,71 @@ class SessionManager {
             sent: 0
         };
 
+        this.hasShutdown = false;
+
+        this.ticks = 0;
+        this.lastMeasure = -1;
+
+        this.startTime = -1;
+
         this.sessions = new Map();
 
         this.offlineMessageHandler = {};
     }
 
-    constructor(server, socket){
+    constructor(server, socket) {
         this.initVars();
 
         this.server = server;
         this.socket = socket;
 
+        this.startTime = Date.now();
+
         this.offlineMessageHandler = new OfflineMessageHandler(this);
-        this.datagramHandler = new DatagramHandler(this);
+
+        this.start();
+    }
+
+    start(){
+        this.tickProcessor();
+    }
+
+    shutdown(){
+        this.hasShutdown = true;
+    }
+
+    tickProcessor(){
+        this.lastMeasure = Date.now();
+
+        let int = setInterval(() => {
+            if(!this.hasShutdown){
+                this.tick();
+            }else{
+                clearInterval(int);
+            }
+        }, RAKNET_TICK_LENGTH * 1000);
+    }
+
+    tick(){
+        let time = Date.now();
+
+        for(let [,session] of this.sessions){
+            session.update(time);
+        }
+
+        if((this.ticks % RAKNET_TPS) === 0){
+            this.lastMeasure = time;
+            this.bytes.sent = 0;
+            this.bytes.received = 0;
+
+            //if blocked check timeout
+        }
+
+        ++this.ticks;
+    }
+
+    getTimeSinceStart(){
+        return Date.now() - this.startTime;
     }
 
     getPort(){
@@ -59,42 +115,68 @@ class SessionManager {
         };
     }
 
-    sendPacket(packet, session){
+    sendPacket(packet, address, port){
         packet.encode();
-        this.bytes.sent += this.socket.getSocket().send(packet.getBuffer(), 0, packet.getBuffer().length, session.getPort(), session.getAddress());
+        if(address instanceof Session){
+            this.bytes.sent += this.socket.getSocket().send(packet.getBuffer(), 0, packet.getBuffer().length, address.getPort(), address.getAddress());
+            //this.getLogger().debug("Sent "+packet.constructor.name+"("+packet.stream.buffer.toString("hex")+") to "+address);
+        }else{
+            this.bytes.sent += this.socket.getSocket().send(packet.getBuffer(), 0, packet.getBuffer().length, port, address);
+            //this.getLogger().debug("Sent "+packet.constructor.name+"("+packet.stream.buffer.toString("hex")+") to "+address+":"+port);
+        }
     }
 
     createSession(address, port, clientId, mtuSize){
-        let session = new Session(address, port, clientId, mtuSize);
+        let session = new Session(this, address, port, clientId, mtuSize);
         this.sessions.set(address + ":" + port, session);
         return session;
     }
 
     sessionExists(address, port){
-        if(address instanceof TempSession || address instanceof Session) return this.sessions.has(address.toString());
-        else return this.sessions.has(address + ":" + port);
+        return this.sessions.has(address + ":" + port);
+    }
+
+    removeSession(session, reason = "unknown"){
+        session.close();
+        //PocketNode.tell(new InterfaceMessage("playerDisconnect", {reason:reason}))
+        this.getLogger().debug("Removed "+session+" because of "+reason);
+        return this.sessions.delete(session.toString());
     }
 
     getSession(address, port){
-        if(this.sessionExists(address, port)){
-            return this.sessions.get(address + ":" + port);
+        if(this.sessionExists(address, port)) return this.sessions.get(address + ":" + port);
+        else return null;
+    }
+
+    handle(packetId, stream, ip, port){
+        let session = this.getSession(ip, port);
+
+        if(session === null){
+            let packet = this.server.getPacketPool().getPacket(packetId);
+            if(packet !== null && (packet = new packet(stream))){
+                if(packet instanceof OfflineMessage){
+                    packet.decode();
+                    if(packet.validMagic()){
+                        if(!this.offlineMessageHandler.handle(packet, ip, port)){
+                            this.getLogger().debug("Received unhandled offline message " + packet.constructor.name + " from " + session);
+                        }
+                    }else{
+                        this.getLogger().debug("Received invalid message from " + session + ":", "0x" + packet.getBuffer().toString("hex"));
+                    }
+                }
+            }
         }else{
-            return false;
-        }
-    }
-
-    handle(packet, session){
-        packet.decode();
-        if(packet instanceof OfflineMessage){
-            this.offlineMessageHandler.handle(packet, session);
-        }else if(packet instanceof Datagram){
-            this.datagramHandler.handle(packet, session);
-        }
-    }
-
-    tick(){
-        for(let [,session] of this.sessions){
-            console.log(session)
+            if((packetId & BITFLAG.VALID) === 0){
+                this.getLogger().debug("Ignored non-connected message for " + session + " due to session already opened");
+            }else{
+                if(packetId & BITFLAG.ACK){
+                    session.handlePacket(new ACK(stream));
+                }else if(packetId & BITFLAG.NAK){
+                    session.handlePacket(new NACK(stream));
+                }else{
+                    session.handlePacket(new Datagram(stream));
+                }
+            }
         }
     }
 }
